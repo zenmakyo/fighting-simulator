823856a930ab478a7a9e9939798e2d4e22424a74
zenmakyo <108194461+zenmakyo@users.noreply.github.com>
2026-04-10 04:26:58 +0900

    Update data.js

--- data.js
+++ data.js
@@ -10,6 +10,16 @@ const weaponList = [
     { name: "不乱：金狐の絵筆", grade: 5, baseAtk: 10, ability: "不乱" }
 ];
 
+// 防具リスト
+const armorList = [
+    { name: "未選択", grade: 0, baseDef: 0, ability: "なし" },
+    { name: "蚩尤ノ冥煌鎧", grade: 10, baseDef: 245, ability: "猛皇" },
+    { name: "飛翔の武冠", grade: 9, baseDef: 35, ability: "猛将" },
+    { name: "鏖魔瞳", grade: 10, baseDef: 500, ability: "修羅" },
+    { name: "虎乗の覇気", grade: 6, baseDef: 10, ability: "虎咆" }
+    // ここに必要な防具をどんどん追加してください
+];
+
 // 武器アビリティリスト
 const weaponAbilityList = [
     { name: "未選択" },
