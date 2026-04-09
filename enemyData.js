583ee82148ddf1747ecede0a5f09b923ae635935
zenmakyo <108194461+zenmakyo@users.noreply.github.com>
2026-04-10 04:42:24 +0900

    Create enemyData.js

---
+++ enemyData.js
@@ -0,0 +1,9 @@
+//  敵幻獣ステータス（ここを自由に追加・編集してください）
+const enemyData = [
+    { name: "Lv.200:闇黒龍", attr: "龍", sta: 202100, atk: 9000, def: 5000, ability: "高揚" },
+    { name: "Lv.150:叛逆の断罪者", attr: "魔", sta: 87000, atk: 7500, def: 3200, ability: "強打" },
+    { name: "Lv.135:盲鬼ト浮鬼", attr: "獣", sta: 69000, atk: 7360, def: 3000, ability: "強打"},
+    { name: "Lv.120:砂漠の亡者", attr: "魔", sta: 55000, atk: 7250, def: 2880, ability: "高揚"},
+    { name: "Lv.115:終焉の指揮者", attr: "魔", sta: 48300, atk: 6420, def: 2640, ability: "強打"},
+    { name: "Lv.109:ポセイドン", attr: "霊", sta: 43000, atk: 6000, def: 2350, ability: "強打"},
+];
