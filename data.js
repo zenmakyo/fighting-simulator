d06c4684f8b32e1214fad9c0b2dc6f0ef06454ce
zenmakyo <108194461+zenmakyo@users.noreply.github.com>
2026-04-10 02:07:48 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,8 @@
+// 武器リスト
+const weaponList = [
+    { name: "未装備", grade: 0, baseAtk: 0, ability: "なし" },
+    { name: "疾風：颶風連鎖刃", grade: 10, baseAtk: 520, ability: "疾風" },
+    { name: "紅舞：ブラッディローズ", grade: 10, baseAtk: 780, ability: "紅舞" },
+    { name: "紅舞：衝天ノ神楽鉾", grade: 9, baseAtk: 250, ability: "紅舞" },
+    { name: "紅舞：丹青の打楽器", grade: 3, baseAtk: 5, ability: "紅舞" }
+];
