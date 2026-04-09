f4c6b761a80dbcaeb07b19e99cc2e19ed2c8a876
zenmakyo <108194461+zenmakyo@users.noreply.github.com>
2026-04-10 04:41:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-// 1. データベース（ここを自由に追加・編集してください）
-const enemyData = [
-    { name: "Lv.200:闇黒龍", attr: "龍", sta: 202100, atk: 9000, def: 5000, ability: "高揚" },
-    { name: "Lv.150:叛逆の断罪者", attr: "魔", sta: 87000, atk: 7500, def: 3200, ability: "強打" },
-    { name: "Lv.135:盲鬼ト浮鬼", attr: "獣", sta: 69000, atk: 7360, def: 3000, ability: "強打"},
-    { name: "Lv.120:砂漠の亡者", attr: "魔", sta: 55000, atk: 7250, def: 2880, ability: "高揚"},
-    { name: "Lv.115:終焉の指揮者", attr: "魔", sta: 48300, atk: 6420, def: 2640, ability: "強打"},
-    { name: "Lv.109:ポセイドン", attr: "霊", sta: 43000, atk: 6000, def: 2350, ability: "強打"},
-    // ここに同じ形式でどんどん追加できます
-];
-
-// 2. 討伐幻獣名の箱
+// 1. 討伐幻獣名の箱
 // A. メニューの開閉（ボタンを押したとき）
 function toggleSearchMenu() {
     const menu = document.getElementById('targetMenu');
@@ -99,7 +88,7 @@ function updateEnemyStats() {
 }
 
 
-//3.自分の幻獣ステータスとリストの設定
+// 2.自分の幻獣ステータスとリストの設定
 // 詳細エリアの開閉
 function toggleDetail(num) {
     const detail = document.getElementById(`detail-${num}`);
