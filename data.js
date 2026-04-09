331647bbba8ce1ad27f72c719bae9b085be9773b
zenmakyo <108194461+zenmakyo@users.noreply.github.com>
2026-04-10 02:26:00 +0900

    Update data.js

--- data.js
+++ data.js
@@ -4,5 +4,8 @@ const weaponList = [
     { name: "疾風：颶風連鎖刃", grade: 10, baseAtk: 520, ability: "疾風" },
     { name: "紅舞：ブラッディローズ", grade: 10, baseAtk: 780, ability: "紅舞" },
     { name: "紅舞：衝天ノ神楽鉾", grade: 9, baseAtk: 250, ability: "紅舞" },
-    { name: "紅舞：丹青の打楽器", grade: 3, baseAtk: 5, ability: "紅舞" }
+    { name: "紅舞：丹青の打楽器", grade: 3, baseAtk: 5, ability: "紅舞" },
+    { name: "不乱：天巴之御佩刀", grade: 9, baseAtk: 310, ability: "不乱" },
+    { name: "不乱：仁双応", grade: 9, baseAtk: 220, ability: "不乱" },
+    { name: "不乱：金狐の絵筆", grade: 5, baseAtk: 10, ability: "不乱" }
 ];
