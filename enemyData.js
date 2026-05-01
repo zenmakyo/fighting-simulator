3b258414db122a08b73f7742a0be91a6a5e1f377
zenmakyo <108194461+zenmakyo@users.noreply.github.com>
2026-05-01 16:13:08 +0900

    Update enemyData.js

--- enemyData.js
+++ enemyData.js
@@ -6,4 +6,6 @@ const enemyData = [
     { name: "Lv.120:砂漠の亡者", attr: "魔", sta: 55000, atk: 7250, def: 2880, ability: "高揚"},
     { name: "Lv.115:終焉の指揮者", attr: "魔", sta: 48300, atk: 6420, def: 2640, ability: "強打"},
     { name: "Lv.109:ポセイドン", attr: "霊", sta: 43000, atk: 6000, def: 2350, ability: "強打"},
+    { name: "Lv.99:死を告げる者", attr: "無", sta: 41111, atk: 5800, def: 2200, ability: "なし"},
+    { name: "Lv.90:疎まれし者", attr: "龍", sta: 26500, atk: 3600, def: 1200, ability: "強打"},
 ];
