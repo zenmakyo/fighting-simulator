c6cae70c9b1c851aafbe9a7a38be41cc9c4f6fbb
zenmakyo <108194461+zenmakyo@users.noreply.github.com>
2026-05-01 17:05:43 +0900

    Update enemyData.js

--- enemyData.js
+++ enemyData.js
@@ -8,4 +8,10 @@ const enemyData = [
     { name: "Lv.109:ポセイドン", attr: "霊", sta: 43000, atk: 6000, def: 2350, ability: "強打"},
     { name: "Lv.99:死を告げる者", attr: "無", sta: 41111, atk: 5800, def: 2200, ability: "なし"},
     { name: "Lv.90:疎まれし者", attr: "龍", sta: 26500, atk: 3600, def: 1200, ability: "強打"},
+    { name: "Lv.83:刑罰を記す者", attr: "魔", sta: 25543, atk: 3420, def: 1440, ability: "強打"},
+    { name: "Lv.79:ロックイーター", attr: "地", sta: 10165, atk: 1240, def: 650, ability: "なし"},
+    { name: "Lv.72:カトブレパス", attr: "龍", sta: 14420, atk: 1170, def: 380, ability: "なし"},
+    { name: "Lv.66:海太郎", attr: "霊", sta: 6800, atk: 1200, def: 460, ability: "なし"},
+    { name: "Lv.60:意思ある肖像", attr: "無", sta: 7400, atk: 1000, def: 350, ability: "なし"},
+    
 ];
